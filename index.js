53a78c6dec4aff7a0e099b9cd87e84bb8c884300
Aleksi Merebashvili <aleksimerebashvili@gmail.com>
2023-11-03 20:52:30 +0400

    create models/blog.js file, export schema in its own module

--- index.js
+++ index.js
@@ -3,17 +3,9 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import config from './utils/config.js';
 import logger from './utils/logger.js';
+import Blog from './models/blog.js';
 
 const app = express();
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl)

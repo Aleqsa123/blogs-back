1a7c276d0f806249bc09f8d80bb4adc8a4c13da9
Aleksi Merebashvili <aleksimerebashvili@gmail.com>
2023-11-03 21:11:31 +0400

    4.2 Blog list, step2, Refactor the application into separate modules

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import config from './utils/config.js';
 import logger from './utils/logger.js';
-import Blog from './models/blog.js';
+import notesRouter from './controllers/blogs.js';
 
 const app = express();
 
@@ -13,24 +13,7 @@ logger.info("Successfully connected to mongoDB");
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use("/api/blogs", notesRouter);
 
 const PORT = config.PORT;
 app.listen(PORT, () => {

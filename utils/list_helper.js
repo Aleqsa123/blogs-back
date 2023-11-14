eb43b96b782ef10f2c04a60edf2bffd47666c380
Aleksi Merebashvili <aleksimerebashvili@gmail.com>
2023-11-14 20:19:50 +0400

    finish 4.4: helper functions and unit tests, step2, tested bloglikes of zero, one and more bloglists

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,11 @@
 const dummy = (blogs) => {
     return 1;
   }
-  
-  export default { dummy };
+
+const  totalLikes  = (blogs) => {
+  if(blogs.length <= 0){ return "zero"}
+  else if(blogs.length == 1){ return Number(blogs.map( blog => blog.likes ))}
+  else return blogs.reduce((sum, blog)=>{ return sum + blog.likes}, 0);
+}
+
+  export default { dummy, totalLikes };

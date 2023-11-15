808277442d42f89db71907d924d779e82e4ed723
Aleksi Merebashvili <aleksimerebashvili@gmail.com>
2023-11-15 23:30:05 +0400

    finish 4.5*: helper functions and unit tests, step3, create and test favouriteBlog function

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,10 +2,23 @@ const dummy = (blogs) => {
     return 1;
   }
 
-const  totalLikes  = (blogs) => {
+const  totalLikes = (blogs) => {
   if(blogs.length <= 0){ return "zero"}
   else if(blogs.length == 1){ return Number(blogs.map( blog => blog.likes ))}
   else return blogs.reduce((sum, blog)=>{ return sum + blog.likes}, 0);
 }
 
-  export default { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  const maxLikes = (obj) => {
+    return obj.likes === Math.max(...blogs.map(blog => blog.likes))
+  };
+  const maxBlog = blogs.find(maxLikes);
+  return {title: maxBlog.title,
+          author: maxBlog.author,
+          likes: maxBlog.likes
+          }
+}
+
+  export default { dummy, 
+                   totalLikes,
+                   favoriteBlog };
